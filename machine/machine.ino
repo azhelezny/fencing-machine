48bc7303dbca2fff1a6bdcdceb4adefbf8c90f42
azhelezny <andrey.zhelezny@gmail.com>
2016-09-09 13:24:18 +0300

    LED test added

--- machine/machine.ino
+++ machine/machine.ino
@@ -1,9 +1,36 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <SPI.h>
 
+int dataPin = 11;
+int clockPin = 13;
+int lachPin = 8;
+
+byte bt[8] = {B11111111, B11111111, B11111111, B11111111, B11111111, B11111111, B11111111, B11111111};
+
+void setup()
+{
+  pinMode(dataPin, OUTPUT);
+  pinMode(lachPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+
+  
+  digitalWrite(lachPin, LOW);
+  for(int i=0;i<8;i++)
+  {
+    shiftOut(dataPin, clockPin, LSBFIRST, B00000000);
+  }  
+  digitalWrite(lachPin, HIGH);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void writeByte(byte * value)
+{
+  
+}
 
+
+void loop()
+{
+  digitalWrite(lachPin, LOW);
+  for(int i=7;i>=0;i--)
+    shiftOut(dataPin, clockPin, LSBFIRST, bt[i]);
+  digitalWrite(lachPin, HIGH);
 }
